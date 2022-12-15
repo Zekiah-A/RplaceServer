053a6333a52d34ba91fde2e0a7705ce5667bf6c1
Zekiah-A <zekiahamoako@outlook.com>
2022-12-15 01:13:12 +0000

    Code clean

--- RplaceServer/TimelapseGeneration/RateLimiter.cs
+++ RplaceServer/TimelapseGeneration/RateLimiter.cs
@@ -4,34 +4,34 @@ namespace PlaceHttpsServer;
 
 public class RateLimiter
 {
-    private readonly Dictionary<IPAddress, DateTime> RegisteredIPs;
-    private readonly TimeSpan LimitPeriod;
+    private readonly Dictionary<IPAddress, DateTime> registeredIPs;
+    private readonly TimeSpan limitPeriod;
     
-    public RateLimiter(TimeSpan limitPeriod)
+    public RateLimiter(TimeSpan limit)
     {
-        LimitPeriod = limitPeriod;
-        RegisteredIPs = new Dictionary<IPAddress, DateTime>();
+        limitPeriod = limit;
+        registeredIPs = new Dictionary<IPAddress, DateTime>();
     }
 
     public bool IsAuthorised(IPAddress address, bool extendIfNot = false)
     {
-        if (!RegisteredIPs.ContainsKey(address) || !RegisteredIPs.TryGetValue(address, out var startDate))
+        if (!registeredIPs.ContainsKey(address) || !registeredIPs.TryGetValue(address, out var startDate))
         {
-            RegisteredIPs.Add(address, DateTime.Now);
+            registeredIPs.Add(address, DateTime.Now);
             return false;
         }
 
-        if (DateTime.Now - startDate < LimitPeriod)
+        if (DateTime.Now - startDate < limitPeriod)
         {
             if (extendIfNot)
             {
-                RegisteredIPs[address] = DateTime.Now;
+                registeredIPs[address] = DateTime.Now;
             }
             
             return false;
         }
 
-        RegisteredIPs.Remove(address);
+        registeredIPs.Remove(address);
         return true;
     }
 }
