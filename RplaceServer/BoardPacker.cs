91ba50a7018619085cb9f1017c79a42a3b1aaa43
Zekiah-A <zekiahamoako@outlook.com>
2022-12-16 00:48:23 +0000

    Spanify packboard

--- RplaceServer/BoardPacker.cs
+++ RplaceServer/BoardPacker.cs
@@ -9,43 +9,45 @@ public static class BoardPacker
     {
         
         var metadataLength = 4 + (palette?.Count ?? 0) * 4 + 2;
-        var packedBoard = new byte[board.Length + metadataLength];
-        Array.Copy(board, packedBoard, board.Length);
+        var packedBoard = new Span<byte>(new byte[board.Length + metadataLength]);
+        board.CopyTo(packedBoard);
 
         var iteration = board.Length;
-        BinaryPrimitives.WriteUInt32BigEndian(packedBoard.AsSpan()[iteration..(iteration + 4)], (uint) boardWidth);
+        BinaryPrimitives.WriteUInt32BigEndian(packedBoard[iteration..(iteration + 4)], (uint) boardWidth);
         iteration += 4;
 
         if (palette is not null)
         {
             for (var colour = board.Length + 4; colour < packedBoard.Length; colour++)
             {
-                BinaryPrimitives.WriteUInt32BigEndian(packedBoard.AsSpan()[iteration..(iteration + 4)], (uint) palette[colour]);
+                BinaryPrimitives.WriteUInt32BigEndian(packedBoard[iteration..(iteration + 4)], (uint) palette[colour]);
                 iteration += 4;
             }
         }
         
-        BinaryPrimitives.WriteUInt16BigEndian(packedBoard.AsSpan()[iteration..(iteration + 2)], (ushort) metadataLength);
+        BinaryPrimitives.WriteUInt16BigEndian(packedBoard[iteration..(iteration + 2)], (ushort) metadataLength);
         
-        return packedBoard;
+        return packedBoard.ToArray();
     }
 
-    public static UnpackedBoard UnpackBoard(byte[] packedBoard)
+    public static UnpackedBoard UnpackBoard(byte[] packed)
     {
+        var packedBoard = new Span<byte>(packed);
+        
         var metadataLength =
-            BinaryPrimitives.ReadUInt16BigEndian(packedBoard.AsSpan()[(packedBoard.Length - 2)..packedBoard.Length]);
+            BinaryPrimitives.ReadUInt16BigEndian(packedBoard[^2..packedBoard.Length]);
         var boardLength = packedBoard.Length - metadataLength;
 
-        var boardWidth = BinaryPrimitives.ReadUInt32BigEndian(packedBoard.AsSpan()[boardLength..(boardLength + 4)]);
+        var boardWidth = BinaryPrimitives.ReadUInt32BigEndian(packedBoard[boardLength..(boardLength + 4)]);
 
         var palette = new List<int>();
         for (var i = boardLength + 2; i < packedBoard.Length - 2; i++)
         {
-            palette.Add((int) BinaryPrimitives.ReadUInt32BigEndian(packedBoard.AsSpan()[i..(i+4)]));
+            palette.Add((int) BinaryPrimitives.ReadUInt32BigEndian(packedBoard[i..(i+4)]));
         }
 
         var board = new byte[boardLength];
-        Array.Copy(packedBoard, board, boardLength);
+        packedBoard[..boardLength].CopyTo(board);
 
         return new UnpackedBoard(board, (int) boardWidth, palette);
     }
