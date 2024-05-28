eac0377b4e7d538af0ce1dbc2f69da291c6d5eee
Zekiah-A <zekiahamoako@outlook.com>
2024-05-29 00:38:01 +0100

    Canvas user response / instance user nullable chatName

---
+++ HTTPOfficial/ApiModel/CanvasUserResponse.cs
@@ -0,0 +1,4 @@
+namespace HTTPOfficial.ApiModel;
+
+public record CanvasUserResponse(int Id, int UserIntId, int InstanceId, int? AccountId,
+    string? ChatName, DateTime LastJoined, int PixelsPlaced, int PlayTimeSeconds);
